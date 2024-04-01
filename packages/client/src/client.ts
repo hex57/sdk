9effb21e5b6f7dd26a1d41f10e7d2298d7b80f1e
Leah Lundqvist <leah@pigeon.sh>
2024-04-01 23:21:27 +0000

    🔧 feat/SDK-11
    “Refines Hex57 and PasskeyClient classes with API versioning and URL handling.
    ===
    • :Cog: Added `APIVersion` enum and `Hex57Options` interface for API versioning.
    • :Link2: Improved `Hex57` URL handling, using `apiVersion` and `apiBase`.
    • :Inherit: `PasskeyClient` now extends `Hex57Options` for streamlined options passing.
    
    ---
    
    ”
    
    Read more: https://pierre.co/0x57/sdk/feat/SDK-11

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -1,73 +1,98 @@
 /* eslint-env node */
 
 export enum RequestMethod {
-	GET = "GET",
-	POST = "POST",
-	PATCH = "PATCH",
-	PUT = "PUT",
-	DELETE = "DELETE",
+  GET = "GET",
+  POST = "POST",
+  PATCH = "PATCH",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
+
+export enum APIVersion {
+  ALPHA = "alpha",
+}
+
+export interface Hex57Options {
+  apiVersion?: APIVersion;
+  apiBase?: string;
 }
 
 export class Hex57 {
-	version: string;
-	#key: string;
+  readonly #apiUrl: string;
+  #key: string;
 
-	constructor(key: string, version = "alpha") {
-		this.#key = key;
-		this.version = version;
-	}
+  constructor(
+    key: string,
+    {
+      apiVersion = APIVersion.ALPHA,
+      apiBase = "https://www.0x57.com/api",
+    }: Hex57Options = {}
+  ) {
+    this.#key = key;
+    this.#apiUrl = `${apiBase}/${apiVersion}`;
+  }
 
-	set key(key: string) {
-		this.#key = key;
-	}
+  set key(key: string) {
+    this.#key = key;
+  }
 
-	async request<T>(
-		method: RequestMethod,
-		url: string,
-		body?: Record<string, unknown>
-	) {
-		const options: RequestInit = {
-			headers: {
-				"Content-Type": "application/json",
-				"Accept": "application/json",
-				"Authorization": this.#key,
-			},
-			method,
-		};
+  url(path: string) {
+    // im not stoked on this since there are ways to break this. for example if you pass alpha/ as the version
+    // but it feels unnecessary to pull in an entire library for safe url joining
+    // especially since this is mostly a debug feature
+    if (!path.startsWith("/")) {
+      return `${this.#apiUrl}/${path}`;
+    }
 
-		if (body != null) {
-			options.body = JSON.stringify(body);
-		}
+    return `${this.#apiUrl}${path}`;
+  }
 
-		const response = await fetch(
-			`https://www.0x57.dev/api/${this.version}/${url}`,
-			options
-		);
+  async request<T>(
+    method: RequestMethod,
+    url: string,
+    body?: Record<string, unknown>
+  ) {
+    const options: RequestInit = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: this.#key,
+      },
+      method,
+    };
 
-		if (response.status === 500) {
-			throw new Error("0x57 Service returned a 500");
-		}
+    if (body != null) {
+      options.body = JSON.stringify(body);
+    }
 
-		if (!response.ok) {
-			await this.getErrors(response);
-		}
+    const response = await fetch(this.url(url), options);
 
-		return response;
-	}
+    if (response.status === 500) {
+      throw new Error("0x57 Service returned a 500");
+    }
 
-	async getErrors(response: Response) {
-		const errors = (await response.json()) as unknown;
+    if (!response.ok) {
+      await this.getErrors(response);
+    }
 
-		if (typeof errors === "object" && errors != null && "error" in errors) {
-			if (typeof errors.error === "string") {
-				throw new Error(`${response.status}: ${errors.error}`);
-			} else if (Array.isArray(errors.error)) {
-				throw new Error(`${response.status}: ${errors.error.join(", ")}`);
-			}
-		} else if (typeof errors === "string") {
-			throw new Error(`${response.status}: ${errors}`);
-		} else {
-			throw new Error(`Unknown ${response.status} error encountered`);
-		}
-	}
+    return response;
+  }
+
+  async getErrors(response: Response) {
+    const errors = (await response.json()) as unknown;
+
+    if (typeof errors === "object" && errors != null && "error" in errors) {
+      if (typeof errors.error === "string") {
+        throw new Error(`${response.status}: ${errors.error}`);
+      }
+
+      if (Array.isArray(errors.error)) {
+        throw new Error(`${response.status}: ${errors.error.join(", ")}`);
+      }
+    } else if (typeof errors === "string") {
+      throw new Error(`${response.status}: ${errors}`);
+    } else {
+      throw new Error(`Unknown ${response.status} error encountered`);
+    }
+  }
 }
