c8e3798ff83330bbff8659b16956a4eba062bd96
Leah Lundqvist <leah@pigeon.sh>
2024-07-07 01:25:54 +0200

    mock-webauthn: remove reference code

--- packages/mock-webauthn/src/assertion.ts
+++ packages/mock-webauthn/src/assertion.ts
@@ -1,9 +1,3 @@
-// type AssertionOptions struct {
-//     Challenge        []byte   `json:"challenge,omitempty"`
-//     AllowCredentials []string `json:"allowCredentials,omitempty"`
-//     RelyingPartyID   string   `json:"rpId,omitempty"`
-// }
-
 export interface AssertionOptions {
     challenge: ArrayBuffer;
     allowCredentials: string[];
